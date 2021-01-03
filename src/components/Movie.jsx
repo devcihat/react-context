b94c94e791c6056220fd642bbbbec56a78c340ed
devcihat <cihattsungu@outlook.com>
2021-01-03 18:43:12 +0300

    1 file modified

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,28 +3,28 @@ import React from "react";
 const Movie = ({ name, price, image }) => {
   return (
     <div className="container">
-      <div class="row">
-        <div class="col-xs-4">
-          <div class="card mb-3 text-center">
-            <div class="card-body">
+      <div className="row">
+        <div className="col-xs-4">
+          <div className="card mb-3 text-center">
+            <div className="card-body">
               <img className="img-fluid mb-3" src={image} />
-              <h5 class="card-title">{name}</h5>
-              <p class="card-text">{price}</p>
+              <h5 className="card-title">{name}</h5>
+              <p className="card-text">{price}</p>
               <div className="mx-auto">
-                <a href="#" class="btn btn-success mx-1">
+                <a href="#" className="btn btn-success mx-1">
                   Hemen İzle
                 </a>
-                <a href="#" class="btn btn-secondary">
+                <a href="#" className="btn btn-secondary">
                   İndir
                 </a>
               </div>
             </div>
             <div className="mx-auto">
               <i style={{ color: "yellow" }} className="fas fa-star" />
-              <i style={{ color: "yellow" }} class="fas fa-star" />
-              <i style={{ color: "yellow" }} class="fas fa-star" />
-              <i style={{ color: "yellow" }} class="fas fa-star" />
-              <i style={{ color: "yellow" }} class="fas fa-star" />
+              <i style={{ color: "yellow" }} className="fas fa-star" />
+              <i style={{ color: "yellow" }} className="fas fa-star" />
+              <i style={{ color: "yellow" }} className="fas fa-star" />
+              <i style={{ color: "yellow" }} className="fas fa-star" />
             </div>
           </div>
         </div>
