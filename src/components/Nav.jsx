9d99384739231264cc3c6c88d57ec528c5fd2b1b
devcihat <cihattsungu@outlook.com>
2021-01-03 18:01:10 +0300

    1 file modified

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
     <div className="text-center  ">
       <nav className="navbar navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand ms-4">Dev Cihat</a>
+          <a className="navbar-brand ms-4">LOGO</a>
           <p className="text-white pt-3">List of Movies: {movies.length}</p>
         </div>
       </nav>
